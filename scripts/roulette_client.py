f755806e3ed89098ed33848da8954604663ab561
KirillPotapov <72515232+KirillPotapov@users.noreply.github.com>
2020-12-17 20:14:07 +0300

    Add files via upload

---
+++ scripts/roulette_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import rospy
+import string
+import random
+from ROS_lab2.srv import *
+
+
+def roulette_client(clr, numb):
+    rospy.wait_for_service('roll_roulette')
+    try:
+        roll_roulette = rospy.ServiceProxy('roll_roulette', GetRouletteRoll)
+        resp1 = roll_roulette(clr, numb)
+        return resp1.roll_result
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+
+
+if __name__ == "__main__":
+    rospy.init_node('client', anonymous=True)
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+
+        color_list = ['green', 'red', 'black']
+        random.shuffle(color_list)
+        color = color_list[0]
+
+        number = random.randrange(0, 36)
+        print(f"Requesting roll result for: {color} {number}")
+        print(roulette_client(color, number))
+        rate.sleep()
